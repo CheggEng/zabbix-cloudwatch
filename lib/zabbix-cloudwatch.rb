ebb40a91a98314f79128c68e8b29fd8feca0a06b
analogrithems <analogrithems@gmail.com>
2016-01-26 18:14:24 -0800

    Update zabbix-cloudwatch.rb

--- lib/zabbix-cloudwatch.rb
+++ lib/zabbix-cloudwatch.rb
@@ -11,29 +11,30 @@ module ZabbixCloudwatch
     class AwsAccessKeyMissingException < StandardError; end
     class AwsSecretKeyMissingException < StandardError; end
     class BadAWSAccessKeysException < StandardError; end
-
+  
     attr_accessor :options, :aws, :start_time, :end_time, :period, :statistic
-
+  
     def initialize options = {}
       self.options = options
       usage if options.key?"help"
       raise NamespaceArgumentMissingException unless options.key?"namespace"
       raise MetricnameArgumentMissingException unless options.key?"metricname"
-      raise DimensionArgumentMissingException unless options.key?"dimensions"
+      raise DimensionArgumentMissingException unless options.key?"dimension-name"
+      raise DimensionArgumentMissingException unless options.key?"dimension-value"
       self.aws = AWS::CloudWatch.new(get_aws_options).client
     end
 
     def get_aws_options
       raise AwsAccessKeyMissingException unless options.key?"aws-access-key"
       raise AwsSecretKeyMissingException unless options.key?"aws-secret-key"
-      if options.key?("aws-region") and options['aws-region'] != ''
+      if options.key?"aws-region" and options['aws-region'] != ''
         region = options["aws-region"]
       else
         region = 'us-east-1'
       end
       {:access_key_id => options["aws-access-key"], :secret_access_key => options["aws-secret-key"], :region => region}
     end
-
+  
     def set_statistic
       unless options.key?"statistic"
         self.statistic = "Average"
@@ -45,59 +46,50 @@ module ZabbixCloudwatch
         end
       end
     end
-
+  
     def set_time_range
       unless options.key?"monitoring-type"
-        self.start_time = time_five_minutes_ago
-        self.period = 360
+        detailed = false
       else
-        if options["monitoring-type"] =~ /detailed|basic|manual/
-
-          case options["monitoring-type"]
-            when "detailed"
-              self.start_time = time_one_minute_ago
-              self.period = 60
-            when "basic"
-              self.start_time = time_five_minutes_ago
-              self.period = 360
-            when "manual"
-              seconds_ago = options["seconds-ago"].to_i
-
-              self.start_time = (Time.now - (seconds_ago+30)).utc.iso8601
-              self.period = 360
-          end
+        if options["monitoring-type"] =~ /detailed|basic/
+          detailed = true if options["monitoring-type"] == 'detailed'
+          detailed = false if options["monitoring-type"] == 'basic'
         else
-          raise MonitoringTypeArgumentException, "Monitoring type must be either 'detailed', 'basic' or 'manual'. "
+          raise MonitoringTypeArgumentException, "Monitoring type must be either 'detailed' or 'basic'. "
         end
       end
-
+      if detailed
+        self.start_time = time_one_minute_ago
+        self.period = 60
+      else
+        self.start_time = time_five_minutes_ago
+        self.period = 360
+      end
       self.end_time = time_now
     end
-
+  
     def time_now
       Time.now.utc.iso8601
     end
-
+  
     def time_one_minute_ago
-      # Not really 1 minute ago, but adds a bit of buffer for amazon's silliness
+      # Not really 1 minute ago, but adds a bit of buffer for amazon's silliness 
       (Time.now - 90).utc.iso8601
     end
-
+  
     def time_five_minutes_ago
-      # Not really 5 minutes ago, but adds a bit of buffer for amazon's silliness
+      # Not really 5 minutes ago, but adds a bit of buffer for amazon's silliness 
       (Time.now - (60*7+30)).utc.iso8601
     end
-
+  
     def run!
-     # puts options["dimensions"]
-
       test_aws_connectivity
       set_time_range
       set_statistic
       ret = aws.get_metric_statistics({
               :namespace => options["namespace"],
               :metric_name => options["metricname"],
-              :dimensions => eval(options["dimensions"]),
+              :dimensions => [{:name => options["dimension-name"],:value => options["dimension-value"]}],
               :period => period,
               :start_time => start_time,
               :end_time => end_time,
@@ -110,13 +102,13 @@ module ZabbixCloudwatch
         exit 1
       end
     end
-
+  
     def test_aws_connectivity
       begin
         aws.describe_alarms(:max_records => 1)
       rescue
         raise BadAWSAccessKeysException, <<-EOF
-
+  
   You cannot access AWS due to one of the following reasons:
     - The AWS keys provided do not have access to Cloudwatch
     - your server is not synced with NTP
